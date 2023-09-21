2580244160de4388ac7dc020ae3f8ecfa5e6337a
RizalPrabaswara <rizalbaya34@gmail.com>
2023-09-21 09:09:47 +0700

    Fix DB Problem & Some Minor Changes For Model Building to Link with City (Not Done Yet)

--- coba2rizal/src/Model/BuildingModel.php
+++ coba2rizal/src/Model/BuildingModel.php
@@ -18,8 +18,38 @@ class BuildingModel extends AbstractModel
     protected $id_city;
     protected $building_name;
 
-    public function city()
+    public ?CityModel $cityClass = null;
+
+    public function __construct(int $id = 0, string $building_name = '', int $id_city = 0)
     {
-        return $this->BelongsTo(CityModel::class, 'id_city', 'id');
+        // $this->id = $id;
+        // $this->building_name = $building_name;
+        $this->cityClass = new CityModel($id_city, '');
     }
+
+    public function getArrayCopy(): array
+    {
+    }
+
+    // public function exchangeArray(array $data): array
+    // {
+    //     $this->id = ((int) $data['id'] ?? 0);
+    //     $this->building_name = ($data['building_name'] ?? '');
+    //     $this->cityClass = new CityModel();
+    //     $this->cityClass->id = ((int) $data['id'] ?? 0);
+    //     $this->cityClass->city_name = ($data['city_name'] ?? '');
+    // }
+
+    public function toArray()
+    {
+        $attributes = get_object_vars($this);
+        $attributes['id_city'] = $attributes['cityClass']->id;
+        unset($attributes['cityClass']);
+        return $attributes;
+    }
+
+    // public function city()
+    // {
+    //     return $this->BelongsTo(CityModel::class, 'id_city', 'id');
+    // }
 }
